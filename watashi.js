20554eb8775caa7f070affc5f305e3e23275b90b
Mina Eskandar <mina.aiad@ejust.edu.eg>
2022-11-10 19:00:08 -0500

    Add layar class

--- watashi.js
+++ watashi.js
@@ -10,6 +10,8 @@
  * 
  */
 
+
+
 /**
  * Neuron is the basic unit of the network.
  * It has a value and a list of connections to other neurons.
@@ -55,4 +57,58 @@ class Neuron {
 
 }
 
+
+// neural network
+
+// Layer Class
+/**
+ * Layers is a group of neurons
+ * It has 1 input
+ * @param {number} size - The number of neurons in the layer.
+ * 
+ * The layer does not need to know what the weight, incoming,..etc.
+ * All these are stored in each single neuron.
+ * The layer is more like an interface to a group of neurons.
+ * As before, the layer can connect to other layers, but it can also connect to itself.
+ */
+class Layer {
+    constructor(size) {
+        this.size = size;
+        this.neurons = [];
+        this.connectedTo = [];
+        for (let i = 0; i < this.size; i++) {
+            // To research: What values should it hold? and what are initial weights?
+            // Should it be random?
+            let neuron = new Neuron(0,0,[])
+            this.neurons.push(neuron);
+        }
+        // what if layer is connected to itself? or several layers?
+    }
+
+    /**
+     * Connects the layer to another layers.
+     * 
+     * @param {Layer[]} to The array of layers to connect to.
+     */
+    add_connections(to){
+        for (let i = 0; i < this.neurons.length; i++) {
+            for (let j = 0; j < to.length; j++) {
+                let neuron = this.to[j].neurons[i];
+                this.neurons[i].addOutgoingConnection({neuron:neuron, weight:1});
+            }
+        }
+    }
+    // Fires all neurons in the layer
+    fire(){
+        for (let i = 0; i < this.neurons.length; i++) {
+            this.neurons[i].fire();
+        }
+    }
+
+    activate(){
+        for (let i = 0; i < this.neurons.length; i++) {
+            this.neurons[i].activate();
+        }
+    }
+}
 
